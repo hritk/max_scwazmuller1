f7bc45972b526a04990e29f5e6ba2790769a488a
hritk <ritikrj1@gmail.com>
2022-07-31 15:53:18 +0530

    -m

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -16,7 +16,7 @@ function ExpenseItem(props){
 
         <div className="expense-item__description">
         <h2> {props.title}</h2>
-        <div className="expense-item__price"> $22234</div>
+        <div className="expense-item__price"> ${props.amount}</div>
         </div>
     
       
